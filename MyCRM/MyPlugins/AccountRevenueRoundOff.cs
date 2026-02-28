7cbf216e2a3cc6a882b6383d9b70e44752183d93
Utkarsha Ranade <uranade4700@gmail.com>
2026-03-01 02:05:40 +0530

    Adding RevenueRoundOff

---
+++ MyCRM/MyPlugins/AccountRevenueRoundOff.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugins
+{
+    public class AccountRevenueRoundOff : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                Entity account = (Entity)context.InputParameters["Target"];
+
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+                try
+                {
+                    tracingService.Trace(context.Depth.ToString());
+                    if (context.Depth > 1)
+                    {
+                        return;
+                    }
+                    if (account.Attributes.Contains("revenue"))
+                    {
+                        Money revenue = (Money)account.Attributes["revenue"];
+                        decimal roundedRevenue = Math.Round(revenue.Value, 1);
+                        account.Attributes["revenue"] = new Money(roundedRevenue);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("AccountRevenueRoundOff: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ MyCRM/MyPlugins/PreEntityImageDemo.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugins
+{
+    public class PreEntityImageDemo : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+                // Obtain the IOrganizationService instance which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+                try
+                {
+                    // Plug-in business logic goes here.
+                    //if (context.PreEntityImages.Contains("PreImage") && 
+                    //    context.PreEntityImages["PreImage"] is Entity)
+                    //{
+                    //    Entity preImage = context.PreEntityImages["PreImage"];
+                    //    string firstName = string.Empty;
+                    //    if (preImage.Attributes.Contains("firstname"))
+                    //    {
+                    //        firstName = preImage.Attributes["firstname"].ToString();
+                    //    }
+                    //    string lastName = string.Empty;
+                    //    if (preImage.Attributes.Contains("lastname"))
+                    //    {
+                    //        lastName = preImage.Attributes["lastname"].ToString();
+                    //    }
+                    //    tracingService.Trace("Pre-Entity Image - First Name: {0}, Last Name: {1}", firstName, lastName);
+
+                    string modifiedPhone = entity.Attributes["telephone1"].ToString();
+                    Entity preImage = (Entity)context.PreEntityImages["PreImage"];
+                    string originalPhone = preImage.Attributes["telephone1"].ToString();
+
+                    throw new InvalidPluginExecutionException(string.Format("Original Phone: {0}, Modified Phone: {1}", originalPhone, modifiedPhone));
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("PreEntityImageDemo: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
